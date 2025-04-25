c23842436f41de68e1989838ab1a3811697f24d6
adastra711 <harrisonprehn@gmail.com>
2025-04-24 23:57:46 -0500

    Set up proper Azure Functions structure

--- variance-report/api/config/index.js
+++ variance-report/api/config/index.js
@@ -1,13 +1,21 @@
-module.exports = async function (context, req) {
-  context.res = {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: {
-      AZURE_OPENAI_API_KEY: process.env.AZURE_OPENAI_API_KEY,
-      AZURE_OPENAI_ENDPOINT: process.env.AZURE_OPENAI_ENDPOINT,
-      AZURE_OPENAI_DEPLOYMENT: process.env.AZURE_OPENAI_DEPLOYMENT
+const { app } = require('@azure/functions');
+
+app.http('config', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log('HTTP function processed a request.');
+
+        return {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: {
+                AZURE_OPENAI_API_KEY: process.env.AZURE_OPENAI_API_KEY,
+                AZURE_OPENAI_ENDPOINT: process.env.AZURE_OPENAI_ENDPOINT,
+                AZURE_OPENAI_DEPLOYMENT: process.env.AZURE_OPENAI_DEPLOYMENT
+            }
+        };
     }
-  };
-}; 
+}); 
